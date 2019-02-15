c998c68f09bea00bd50d6765fb0c951b9e2e6de6
João Vanzuita <joaovanzuita@me.com>
2019-02-15 13:30:17 -0200

    layout update (form style, left bar size)

--- src/components/AsideContent/blogPost.jsx
+++ src/components/AsideContent/blogPost.jsx
@@ -18,37 +18,11 @@ export default class BlogPost extends Component {
             </div>
             <div className="content-block profile-info-area">
               <section>
-                <p className="title-bar-noeffect">PROFILE</p>
+                <p className="title-bar-noeffect">Working Here</p>
               </section>
               <p className="leftbar-text">
-                (blog)I'm Brazilian, 35 years old, passionate about technology
-                since my teenage years and curious about future developments.
+                will update it soon :)
             </p>
-              <p className="leftbar-text">
-                I have been a software developer for the past eleven years.
-                Nowadays I work as Senior Full Stack Developer.
-            </p>
-            </div>
-            <div className="content-block about-me-area">
-              <section>
-                <p className="title-bar-noeffect">ABOUT ME</p>
-              </section>
-              <p className="leftbar-text">
-                I see work as an activity to make the planet we live on better.
-            </p>
-              <p className="leftbar-text">
-                To make the world a better place I want to create software that
-                enables the digital opportunities of businesses.
-            </p>
-              <p className="leftbar-text">
-                That’s why I’m passionate about road cycling and meditation,
-                keeping mind, body and spirit healthy is the first step to be
-                fine with ourselves and others.
-            </p>
-              <p className="leftbar-text">
-                A balanced life is key to amazing software.
-            </p>
-
             </div>
             <div className="content-block projects-area">
               <section>
