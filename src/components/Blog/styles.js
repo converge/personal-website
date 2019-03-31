a660626ccb19ff47b35303fb896d2a43830adb85
João Vanzuita <joaovanzuita@me.com>
2019-03-30 21:16:12 -0300

    add style component, and css update to post content

---
+++ src/components/Blog/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const BlogTitle = styled.p`
+  margin-top: 40px;
+  font-size: 1em;
+  font-weight: bold;
+  background-color: #4ce6ac;
+  border-radius: 20px;
+  padding: 15px;
+  color: #fff;
+`
+
+export const BlogCategory = styled.p`
+  margin-top: 15px;
+  font-size: 0.9em;
+  color: #a1a1a1;
+`
