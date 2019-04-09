9fff54dc8c9a9f966e49b769d1f8ba70a9034ad6
João Vanzuita <joaovanzuita@me.com>
2019-04-08 22:05:17 -0300

    end to end test to contact/email form

--- __tests__/integration/e2e.test.js
+++ __tests__/integration/e2e.test.js
@@ -1,5 +1,7 @@
+const faker = require('faker')
+
 describe('Google', () => {
-  beforeAll(async () => {
+  beforeEach(async () => {
     await page.goto('http://www.joaovanzuita.me');
   });
 
@@ -19,7 +21,30 @@ describe('Google', () => {
     expect(postTitle).not.toBe(null)
   })
 
+  it('should send test email', async () => {
+    // fill the form with, name:
+    const nameField = await page.waitForXPath('(//form/div/input)[1]')
+    await nameField.type(`${faker.name.firstName()} ${faker.name.lastName()}`)
+    // e-mail
+    const emailField = await page.waitForXPath('(//form/div/input)[2]')
+    await emailField.type(`${faker.internet.email()}`.toLowerCase())
+    // subject
+    const subjectField = await page.waitForXPath('(//form/div/input)[3]')
+    await subjectField.type(`${faker.random.words(3)}`)
+    // message
+    const messageField = await page.waitForXPath("//textarea[@name='msg']")
+    await messageField.type(`${faker.lorem.sentences(5)}`)
+    // submit button
+    const submitButton = await page.waitForXPath("//button[@type='submit'][text()='Submit']")
+    await submitButton.click()
+
+    await page.waitFor(5000)
+    const emailStatus = await page.waitForXPath("//div[text()='Email sent !']")
+    const emailSentMessage = await (await emailStatus.getProperty('textContent')).jsonValue()
+
+    // console.log(text)
+    expect(emailSentMessage).toMatch('Email sent !')
+  }, 20000)
+
 
 });
-
-
