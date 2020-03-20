7387de29aeb09e212d92c78242f673c71b2b837b
João Vanzuita <joao.vanzuita@de.bosch.com>
2020-03-20 20:43:56 +0100

    add link checker to Actions

--- frontend/src/templates/template-jp.js
+++ frontend/src/templates/template-jp.js
@@ -1,27 +1,24 @@
 import * as React from 'react';
-import {graphql} from 'gatsby';
-import {Helmet} from 'react-helmet';
+import { graphql } from 'gatsby';
+import { Helmet } from 'react-helmet';
 import PostContent from '../components/Blog/postContent';
 import HeaderPost from '../components/Header/headerPost';
 
-const Base = ({data}) => (
-  <React.Fragment>
-    <div>
-      {typeof (data)}
-      <Helmet>
-        <title>João Vanzuita - Full Stack Developer</title>
-      </Helmet>
-      <div className="app">
-        <div className="container">
-          <HeaderPost/>
-          <aside>
-            <PostContent blogPost={data}/>
-          </aside>
-          <footer/>
-        </div>
+const Base = ({ data }) => (
+  <div>
+    <Helmet>
+      <title>João Vanzuita - Full Stack Developer</title>
+    </Helmet>
+    <div className="app">
+      <div className="container">
+        <HeaderPost/>
+        <aside>
+          <PostContent blogPost={ data }/>
+        </aside>
+        <footer/>
       </div>
     </div>
-  </React.Fragment>
+  </div>
 );
 
 export const pageQuery = graphql`
